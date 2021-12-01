123096c93d50a2e84c6d823f6ebdb3dc014c7f41
AbuThekky <zakkynasser@outlook.com>
2021-12-01 11:29:36 +0000

    "Create function has been added"

--- OnePieceAPI/src/main/java/com/example/demo/controllers/API_Controller.java
+++ OnePieceAPI/src/main/java/com/example/demo/controllers/API_Controller.java
@@ -1,20 +1,38 @@
 package com.example.demo.controllers;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+import com.example.demo.model.API;
+import com.example.demo.service.API_Service;
 
 @RestController
 
 @RequestMapping(path = "/api")
 public class API_Controller {
 
-	
-	
+	private API_Service service;
+
+	public API_Controller(API_Service service) {
+		super();
+		this.service = service;
+		// TODO Auto-generated constructor stub
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<API> addCharacter(@RequestBody API api) {
+		ResponseEntity<API> response = new ResponseEntity<API>(this.service.addCharacter(api), HttpStatus.CREATED);
+		return response;
 	}
 
 	
 
-
+}
--- OnePieceAPI/src/main/java/com/example/demo/service/API_Service.java
+++ OnePieceAPI/src/main/java/com/example/demo/service/API_Service.java
@@ -5,15 +5,21 @@ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 
 
-
+import com.example.demo.model.API;
+import com.example.demo.repo.API_Repo;
 
 @Service
 public class API_Service {
      
+	private API_Repo repo;
 	
+	public API_Service(API_Repo repo) {
+		this.repo = repo;
+	}
+	
+	public API addCharacter(API api) {
+		return this.repo.save(api);
 	}
 	
 	
-	
-
-
+}
